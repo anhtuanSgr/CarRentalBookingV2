838c5836b14741b3e12d62cb19bccafc66ced0c6
anhtuanSgr <anhtuansgr.se@gmail.com>
2025-12-14 01:10:23 +0700

    refactor: remove unused React imports from CarDetails, Cars, and MyBookings components

--- frontend/src/pages/CarDetails.jsx
+++ frontend/src/pages/CarDetails.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const CarDetails = () => {
   return <div>CarDetails</div>;
 };
--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Cars = () => {
   return <div>Cars</div>;
 };
